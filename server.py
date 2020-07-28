2e68b5ef2520d5ec1c05c1c707ca9c37386cf6e8
rafauu <gliniakr@gmail.com>
2020-07-28 02:04:11 +0200

    handling post request from app with json

--- server.py
+++ server.py
@@ -1,11 +1,11 @@
-from flask import Flask, render_template, request
+from flask import Flask, request, jsonify
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/', methods=['POST'])
 def iotServer():
-    return "aaa"
+    print(request.json)
+    return jsonify({"id":7})
 
 if __name__ == '__main__':
-    #app.run(host='0.0.0.0', port='80', threaded=True)
     app.run(host='0.0.0.0', port=80, threaded=True, use_debugger=False)
